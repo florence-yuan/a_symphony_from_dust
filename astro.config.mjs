797c6d7573ee01cf995bd01418cb7c27d571dc8a
Florence Yuan <narcissus.yuan@gmail.com>
2025-03-29 21:44:43 +0800

    configure for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://florence-yuan.github.io',
+    base: 'a_symphony_from_dust'
+});
